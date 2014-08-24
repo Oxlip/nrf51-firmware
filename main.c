da8932d4c3e40e7028a37fa08acddded2e8ed92c
Samuel Jacob <samueldotj@gmail.com>
2014-08-24 14:36:28 -0700

    Changed device name.

--- main.c
+++ main.c
@@ -40,7 +40,7 @@
 #define LEDBUTTON_LED_PIN_NO            LED_0
 #define LEDBUTTON_BUTTON_PIN_NO         BUTTON_1
 
-#define DEVICE_NAME                     "LedButtonDemo"                           /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME                     "uPlug"                                     /**< Name of device. Will be included in the advertising data. */
 
 #define APP_ADV_INTERVAL                64                                          /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
 #define APP_ADV_TIMEOUT_IN_SECONDS      180                                         /**< The advertising timeout (in units of seconds). */
@@ -562,7 +562,6 @@ int main(void)
     // Enter main loop
     for (;;)
     {
-        app_sched_execute();
         power_manage();
     }
 }
