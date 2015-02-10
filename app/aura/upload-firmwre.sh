580161e067cc9b54771d923d42ebf5e03e420e44
Samuel Jacob <samueldotj@gmail.com>
2015-02-09 20:45:46 -0800

    Remove file from server before uploading to avoid permission issues.

--- app/aura/upload-firmwre.sh
+++ app/aura/upload-firmwre.sh
@@ -4,6 +4,8 @@ spawn sftp firmware@nuton.in
 expect \"password\"
 send \"binaryRocks\r\"
 expect \"sftp>\"
+send \"rm upload/aura_s110.bin\r\"
+expect \"sftp>\"
 send \"put _build/aura_s110.bin upload\r\"
 expect \"sftp>\"
 send \"bye\r\""
