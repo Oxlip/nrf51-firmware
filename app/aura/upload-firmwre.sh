5913654bea198d982344ae2c975a5c5164e9b939
Samuel Jacob <samueldotj@gmail.com>
2015-02-17 22:03:06 -0800

    Update firmware information on the server once upload is complete.

--- app/aura/upload-firmwre.sh
+++ app/aura/upload-firmwre.sh
@@ -9,3 +9,6 @@ expect \"sftp>\"
 send \"put _build/aura_s110.bin upload\r\"
 expect \"sftp>\"
 send \"bye\r\""
+
+
+curl "https://nuton.in/download/update_firmware?dt=aura&hw=v1&filename=aura_s110.bin&fw_date=`cat _build/build_time.txt`"
