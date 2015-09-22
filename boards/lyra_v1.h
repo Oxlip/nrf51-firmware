20cbae1ee74453a0b244947458f4a5acea05cd11
Samuel Jacob <samueldotj@gmail.com>
2015-09-22 09:22:30 -0700

    Lyra: Fix button configuration.

--- boards/lyra_v1.h
+++ boards/lyra_v1.h
@@ -12,12 +12,12 @@
 
 #define BUTTONS_NUMBER 3
 
-#define BUTTON_START   17
-#define BUTTON_1       17
-#define BUTTON_2       18
-#define BUTTON_3       19
-#define BUTTON_STOP    19
-#define BUTTON_PULL    NRF_GPIO_PIN_PULLDOWN
+#define BUTTON_START   8
+#define BUTTON_1       8
+#define BUTTON_2       24
+#define BUTTON_3       28
+#define BUTTON_STOP    28
+#define BUTTON_PULL    NRF_GPIO_PIN_NOPULL
 #define BUTTON_ACTIVE_STATE  APP_BUTTON_ACTIVE_HIGH
 
 #define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3 }
